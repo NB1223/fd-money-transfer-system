459bc72acee834620a3027d83e19fc2480f0ab4b
= <=>
2026-02-12 09:07:01 +0000

    "Added Transfer Page"

--- frontend/01-money-transfer-system/src/app/app.component.ts
+++ frontend/01-money-transfer-system/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HistoryComponent } from './history/history.component';
 import { RegisterComponent } from './register/register.component';
+import { TransferComponent } from './transfer/transfer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, DashboardComponent, HistoryComponent, RegisterComponent],
+  imports: [RouterOutlet, LoginComponent, DashboardComponent, TransferComponent, HistoryComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/01-money-transfer-system/src/app/transfer.service.ts
+++ frontend/01-money-transfer-system/src/app/transfer.service.ts
@@ -6,4 +6,17 @@ import { Injectable } from '@angular/core';
 export class TransferService {
 
   constructor() { }
+  private transferSuccess = false;
+  
+   transfer(accountNo: number, accNo: number, amount: number): boolean {
+    if (amount >= 0  && accNo!== accountNo) {
+      this.transferSuccess=true;
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+
 }
